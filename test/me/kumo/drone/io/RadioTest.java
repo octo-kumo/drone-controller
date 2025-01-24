e40f6ea21088ea7597fa279195d63c2b39b0df9a
kumo <zy.kumo@gmail.com>
2025-01-24 14:20:50 -0500

    w

--- test/me/kumo/drone/io/RadioTest.java
+++ test/me/kumo/drone/io/RadioTest.java
@@ -5,14 +5,15 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
     @Test
-    public void testRadio() {
+    public void testRadio() throws InterruptedException {
         for (SerialPort commPort : SerialPort.getCommPorts()) {
+            System.out.println(commPort.getPortDescription());
             if ("FT231X USB UART".equals(commPort.getPortDescription())) {
                 System.out.println("Found the radio");
                 System.out.println(commPort.getPortDescription());
                 System.out.println(commPort.openPort());
                 commPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
-//                commPort.writeBytes("Hello, world!".getBytes(), "Hello, world!".length());
+                commPort.writeBytes("Hello, world!".getBytes(), "Hello, world!".length());
                 while (commPort.isOpen()) {
                     byte[] readBuffer = new byte[8];
                     int numRead = commPort.readBytes(readBuffer, readBuffer.length);
@@ -20,6 +21,8 @@ public class RadioTest {
                         System.out.println("Read " + numRead + " bytes");
                         System.out.println(new String(readBuffer, 0, numRead));
                     }
+                    Thread.sleep(100);
+                    commPort.writeBytes("Hello, world!".getBytes(), "Hello, world!".length());
                 }
             }
         }
